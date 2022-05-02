0a548fd5348240c73b75c86d1d5900e3bdd70640
KaBSAlex <mikael.devsystem@hotmail.com>
2022-05-01 23:32:49 -0300

    Feito o menu toggle e algumas alterações na responsividades

---
+++ Script/toggle.js
@@ -0,0 +1,14 @@
+//MENU RESPONSIVO
+
+let show = true;
+
+const menuSection = document.querySelector(".menu_section")
+const menuToggle = menuSection.querySelector(".menu_toggle")
+
+menuToggle.addEventListener("click", () => {
+    document.body.style.overflow = show ? "hidden" : "initial"
+
+    menuSection.classList.toggle("on", show)
+    show = !show;
+})
+
